e17c71392c2fe4e12221392903995e7069aa667e
vudovenko <vladislav.udovenko0@yandex.ru>
2024-04-29 16:09:11 +0500

    index

---
+++ src/main/java/ru/urfu/squadactivityrating/SquadActivityRatingApplication.java
@@ -0,0 +1,13 @@
+package ru.urfu.squadactivityrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadActivityRatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquadActivityRatingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/urfu/squadactivityrating/SquadActivityRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.squadactivityrating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadActivityRatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
